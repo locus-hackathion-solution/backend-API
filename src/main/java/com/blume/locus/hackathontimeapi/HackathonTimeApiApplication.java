c83a5116522394a21f605cab9dd32e1093c3529e
KranthiKumar <kranthi.kumar@walmartlabs.com>
2019-08-03 15:08:16 +0530

    first commit

---
+++ src/main/java/com/blume/locus/hackathontimeapi/HackathonTimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.blume.locus.hackathontimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonTimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blume/locus/hackathontimeapi/HackathonTimeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blume.locus.hackathontimeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackathonTimeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
